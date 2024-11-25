f7003b24c121edb21825070644c7b584e0f0fdea
shrreyakapur <shreyakapur14@gmail.com>
2024-11-25 22:13:31 +0530

    Add files via upload

--- q3 (1).c
+++ q3 (1).c
@@ -1,26 +1,94 @@
-// Recursive Binary Search
-
 #include <stdio.h>
 
-int recursiveBinarySearch(int arr[], int left, int right, int target) {
-    if (left > right) {
-        return -1;
+void add(int A[2][2], int B[2][2], int C[2][2]) {
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            C[i][j] = A[i][j] + B[i][j];
+}
+
+void subtract(int A[2][2], int B[2][2], int C[2][2]) {
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            C[i][j] = A[i][j] - B[i][j];
+}
+
+void traditional_mult(int A[2][2], int B[2][2], int C[2][2]) {
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 2; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+}
+
+void strassen(int A[2][2], int B[2][2], int C[2][2]) {
+    int M1, M2, M3, M4, M5, M6, M7;
+    int temp1, temp2;
+
+    // M1 = (A11 + A22) * (B11 + B22)
+    temp1 = A[0][0] + A[1][1];
+    temp2 = B[0][0] + B[1][1];
+    M1 = temp1 * temp2;
+
+    // M2 = (A21 + A22) * B11
+    temp1 = A[1][0] + A[1][1];
+    M2 = temp1 * B[0][0];
+
+    // M3 = A11 * (B12 - B22)
+    temp2 = B[0][1] - B[1][1];
+    M3 = A[0][0] * temp2;
+
+    // M4 = A22 * (B21 - B11)
+    temp2 = B[1][0] - B[0][0];
+    M4 = A[1][1] * temp2;
+
+    // M5 = (A11 + A12) * B22
+    temp1 = A[0][0] + A[0][1];
+    M5 = temp1 * B[1][1];
+
+    // M6 = (A21 - A11) * (B11 + B12)
+    temp1 = A[1][0] - A[0][0];
+    temp2 = B[0][0] + B[0][1];
+    M6 = temp1 * temp2;
+
+    // M7 = (A12 - A22) * (B21 + B22)
+    temp1 = A[0][1] - A[1][1];
+    temp2 = B[1][0] + B[1][1];
+    M7 = temp1 * temp2;
+
+    // C11 = M1 + M4 - M5 + M7
+    C[0][0] = M1 + M4 - M5 + M7;
+
+    // C12 = M3 + M5
+    C[0][1] = M3 + M5;
+
+    // C21 = M2 + M4
+    C[1][0] = M2 + M4;
+
+    // C22 = M1 - M2 + M3 + M6
+    C[1][1] = M1 - M2 + M3 + M6;
+}
+
+void print_matrix(int M[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++)
+            printf("%d ", M[i][j]);
+        printf("\n");
     }
-    int mid = left + (right - left) / 2;
-    if (arr[mid] == target) {
-        return mid;
-    }
-    if (arr[mid] < target) {
-        return recursiveBinarySearch(arr, mid + 1, right, target);
-    }
-    return recursiveBinarySearch(arr, left, mid - 1, target);
 }
 
 int main() {
-    int arr[] = {1, 3, 5, 7, 9};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 5;
-    int result = recursiveBinarySearch(arr, 0, n - 1, target);
-    printf("Element found at index: %d\n", result);
+    int A[2][2] = {{1, 2}, {3, 4}};
+    int B[2][2] = {{5, 6}, {7, 8}};
+    int C1[2][2], C2[2][2];
+
+    traditional_mult(A, B, C1);
+    printf("Traditional Multiplication Result:\n");
+    print_matrix(C1);
+
+    strassen(A, B, C2);
+    printf("Strassen Multiplication Result:\n");
+    print_matrix(C2);
+
     return 0;
 }
--- q4 (1).c
+++ q4 (1).c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n == 0) return 1;
-    return n * factorial(n - 1);
+int kadane(int arr[], int n) {
+    int max = arr[0];
+    int end = arr[0];
+    for (int i = 1; i < n; i++) {
+        end = end + arr[i];
+        if (max < end) max = end;
+        if (end < 0) end = 0;
+    }
+    return max;
 }
 
 int main() {
-    int n = 5;
-    printf("Factorial of %d is: %d\n", n, factorial(n));
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int max_sum = kadane(arr, n);
+    printf("Maximum sum of subarray: %d\n", max_sum);
     return 0;
 }
